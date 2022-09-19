a6b1de661ba289a7352cc7ab087fa9a70049d238
Muhammad Naufal Aulia <auliamnaufal@gmail.com>
2022-09-19 10:43:44 +0700

    Add functionality to edit and update chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -78,7 +78,15 @@ class ChirpController extends Controller
      */
     public function update(Request $request, Chirp $chirp)
     {
-        //
+        $this->authorize('update', $chirp);
+
+        $validate = $request->validate([
+            'message' => 'required|string|max:255'
+        ]);
+
+        $chirp->update($validate);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
